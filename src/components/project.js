dbe1b7ad8c9547ffd1c4693b66bf2661fde833a3
comav <tsyomav@gmail.com>
2021-12-04 13:23:58 +0200

    0.1.0 production release

--- src/components/project.js
+++ src/components/project.js
@@ -2,7 +2,9 @@ import React from "react";
 import './styles/project.css';
 
 export default function Project() {
+  return(
   <div className="project-wrapper">
-    
+    <p>lol</p>
   </div>
+  )
 }
--- src/screens/projects.js
+++ src/screens/projects.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Project from "../components/project";
 import './styles/projects.css';
 
 export default function Projects() {
   return(
     <div className="projects-wrapper">
       <h1 className="projects-title">My projects</h1>
+      <Project />
+      <Project />
+      <Project />
     </div>
   )
 }
